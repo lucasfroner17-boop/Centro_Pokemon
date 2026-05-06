627c73928335f07289a00723a365c606071b2ec3
lucasfroner17-boop <lucasfroner17@gmail.com>
2026-05-06 11:49:08 -0300

    cadrasto

--- Centro_Pokemon/Centro_Pokemon/Program.cs
+++ Centro_Pokemon/Centro_Pokemon/Program.cs
@@ -34,11 +34,22 @@ namespace Centro_Pokemon
                 Console.WriteLine("\n 3 - apagar Pokemon");
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("\n 4 - sair");
+
+
+                Console.WriteLine("\n digite a opção escolhida: ");
+
                 opcao = int.Parse(Console.ReadLine());
+
+
+
+
+
+
 
                 switch (opcao)
                 {
                     case 1:
+                        cadrastoPokemon(); //abre a fumção
                         break;
                     case 2:
                         break;
@@ -60,11 +71,63 @@ namespace Centro_Pokemon
                 }
 
             }
+        }
+        static void cadrastoPokemon()
+        {
+            Console.Clear(); // limpar a tela 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"
+           
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n digite o nome do seu Pokemon:");
+
+            string nomePokemon = Console.ReadLine();    // sttring = textos 
+            Console.WriteLine("\n digite o tipo do seu Pokemon:");
+
+            string tipoPokemon = Console.ReadLine();    // string
+            Console.WriteLine("\n digite o tamanho do seu Pokemon:");
+
+            double alturaPokemon = double.Parse(Console.ReadLine());  // double = casa decimal 
+
+            Console.WriteLine("\n digite o peso do seu Pokemon:");
+            double pesoPokemon = double.Parse(Console.ReadLine());  // double = casa decim
 
 
+            //fraqueza
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("fraqueza ele tem?:");
+            int qtdfraqueza = int.Parse(Console.ReadLine());
 
+            for (int i = 1; i <= qtdfraqueza; i++)
+            {
+                Console.WriteLine($"fraqueza? {i}: ");  // {i} --- colocar a variavel no texto 
+                string fraquezaPokemon = Console.ReadLine();
+            }
+
+            //fraqueza
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("quantas evoluções ele tem?:");
+            int qtdEvolucao = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtdEvolucao; i++)
+            {
+                Console.WriteLine($"evolução ele tem? {i}: ");  // {i} --- colocar a variavel no texto 
+                string evolucaoPokemon = Console.ReadLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n parabens... pokemon cadastrado");
+            Thread.Sleep(3000);
+            Console.ResetColor();
 
 
         }
     }
-}
+    }
+
